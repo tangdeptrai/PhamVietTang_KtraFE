c0c3f04238cf53049d31c7b3a29832b7b3fcd975
tangdeptrai <tangpham64@gmail.com>
2024-10-08 21:07:02 +0700

    Add project files.

---
+++ PhamVietTang_KtraFE/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhamVietTang_KtraFE.Controllers
+{
+    public class HomeController : Controller
+    {
+       /* public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public ActionResult PhamVietTang_KtraFE()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        /*public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+    }
+}
---
+++ PhamVietTang_KtraFE/Views/Home/PhamVietTang_KtraFE.js
@@ -0,0 +1,38 @@
+﻿function updateQuantity(button, change) {
+    const quantityInput = button.parentNode.querySelector('.quantity');
+    let quantity = parseInt(quantityInput.value) + change;
+
+    if (quantity < 1) quantity = 1;
+    quantityInput.value = quantity;
+
+    updateCartCount();
+    updateTotalPrice();
+}
+
+function updateCartCount() {
+    const quantities = document.querySelectorAll('.quantity');
+    let totalItems = 0;
+
+    quantities.forEach(function (input) {
+        totalItems += parseInt(input.value);
+    });
+
+    document.getElementById('cart-count').innerText = totalItems;
+}
+
+function updateTotalPrice() {
+    const prices = [8480000, 8990000]; // Giá các sản phẩm tương ứng
+    const quantities = document.querySelectorAll('.quantity');
+    let totalPrice = 0;
+
+    quantities.forEach(function (input, index) {
+        totalPrice += parseInt(input.value) * prices[index];
+    });
+
+    // Format số tiền thành kiểu tiền tệ
+    document.getElementById('total-price').innerText = totalPrice.toLocaleString('vi-VN') + '₫';
+}
+
+// Khởi tạo cập nhật ban đầu
+updateCartCount();
+updateTotalPrice();
